36681f5f0368370fcb099d4a8b9fa6170f101267
LeeDongYeun <ledoye@kaist.ac.kr>
2019-11-13 16:13:37 +0900

    initialize

---
+++ biLSTM.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import LSTM, Embedding, Dense, TimeDistributed, Dropout, Bidirectional
+from keras_contrib.layers import CRF
+
+dataPath = 'entity-annotated-corpus/ner_dataset.csv'
+
+data = pd.read_csv(dataPath, encoding="latin1")
+
+data = data.fillna(method="ffill")
+data['Word'] = data['Word'].str.lower()
+
+func = lambda temp: [(w, t) for w, t in zip(temp["Word"].values.tolist(), temp["Tag"].values.tolist())]
+tagged_sentences=[t for t in data.groupby("Sentence #").apply(func)]
+print("전체 샘플 개수: {}".format(len(tagged_sentences)))
+
+sentences, ner_tags = [], [] 
+for tagged_sentence in tagged_sentences: # 47,959개의 문장 샘플을 1개씩 불러온다.
+    sentence, tag_info = zip(*tagged_sentence) # 각 샘플에서 단어들은 sentence에 개체명 태깅 정보들은 tag_info에 저장.
+    sentences.append(list(sentence)) # 각 샘플에서 단어 정보만 저장한다.
+    ner_tags.append(list(tag_info)) # 각 샘플에서 개체명 태깅 정보만 저장한다.
+
+print('샘플의 최대 길이 : %d' % max(len(l) for l in sentences))
+print('샘플의 평균 길이 : %f' % (sum(map(len, sentences))/len(sentences)))
+
+src_tokenizer = Tokenizer(oov_token='OOV') # 모든 단어를 사용하지만 인덱스 1에는 단어 'OOV'를 할당한다.
+src_tokenizer.fit_on_texts(sentences)
+tar_tokenizer = Tokenizer(lower=False) # 태깅 정보들은 내부적으로 대문자를 유지한채로 저장
+tar_tokenizer.fit_on_texts(ner_tags)
+
+vocab_size = len(src_tokenizer.word_index) + 1
+tag_size = len(tar_tokenizer.word_index) + 1
+print('단어 집합의 크기 : {}'.format(vocab_size))
+print('개체명 태깅 정보 집합의 크기 : {}'.format(tag_size))
+
+X_train = src_tokenizer.texts_to_sequences(sentences)
+y_train = tar_tokenizer.texts_to_sequences(ner_tags)
+
+index_to_word = src_tokenizer.index_word
+index_to_ner = tar_tokenizer.index_word
+index_to_ner[0]='PAD'
+
+max_len = 70
+X_train = pad_sequences(X_train, padding='post', maxlen=max_len)
+# X_train의 모든 샘플들의 길이를 맞출 때 뒤의 공간에 숫자 0으로 채움.
+y_train = pad_sequences(y_train, padding='post', maxlen=max_len)
+# y_train의 모든 샘플들의 길이를 맞출 때 뒤의 공간에 숫자0으로 채움.
+
+X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=.2, random_state=777)
+
+y_train = to_categorical(y_train, num_classes=tag_size)
+y_test = to_categorical(y_test, num_classes=tag_size)
+
+print('훈련 샘플 문장의 크기 : {}'.format(X_train.shape))
+print('훈련 샘플 레이블의 크기 : {}'.format(y_train.shape))
+print('테스트 샘플 문장의 크기 : {}'.format(X_test.shape))
+print('테스트 샘플 레이블의 크기 : {}'.format(y_test.shape))
+
+model = Sequential()
+model.add(Embedding(input_dim=vocab_size, output_dim=20, input_length=max_len, mask_zero=True))
+model.add(Bidirectional(LSTM(units=50, return_sequences=True, recurrent_dropout=0.1)))
+model.add(TimeDistributed(Dense(50, activation="relu")))
+crf = CRF(tag_size)
+model.add(crf)
+
+
+model.compile(optimizer="rmsprop", loss=crf.loss_function, metrics=[crf.accuracy])
+history = model.fit(X_train, y_train, batch_size=32, epochs=5, validation_split=0.1, verbose=1)
