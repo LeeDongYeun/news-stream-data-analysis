bf8d33255dee4709891ec8c3536391c1f64d45a4
LeeDongYeun <ledoye@kaist.ac.kr>
2019-11-15 03:25:40 +0900

    ner_tagging (not complete)

--- biLSTM.py
+++ biLSTM.py
@@ -1,14 +1,53 @@
 import pandas as pd
 import numpy as np
+
+from sklearn.model_selection import train_test_split
+from seqeval.metrics import precision_score, recall_score, f1_score, classification_report
+
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from sklearn.model_selection import train_test_split
+
 from tensorflow.keras.utils import to_categorical
+
+from keras.models import load_model
+
 from keras.models import Sequential
 from keras.layers import LSTM, Embedding, Dense, TimeDistributed, Dropout, Bidirectional
 from keras_contrib.layers import CRF
 
-def biLSTM(input_dim, output_dim, input_length, units=50):
+def load_data(dataPath):
+    data = pd.read_csv(dataPath, encoding="latin1")
+    return data
+
+
+def preprocess_data(data):
+    data = data.fillna(method="ffill")
+    data['Word'] = data['Word'].str.lower()
+
+    func = lambda temp: [(w, t) for w, t in zip(temp["Word"].values.tolist(), temp["Tag"].values.tolist())]
+    tagged_sentences=[t for t in data.groupby("Sentence #").apply(func)]
+    print("전체 샘플 개수: {}".format(len(tagged_sentences)))
+
+    sentences, ner_tags = [], [] 
+    for tagged_sentence in tagged_sentences: # 47,959개의 문장 샘플을 1개씩 불러온다.
+        sentence, tag_info = zip(*tagged_sentence) # 각 샘플에서 단어들은 sentence에 개체명 태깅 정보들은 tag_info에 저장.
+        sentences.append(list(sentence)) # 각 샘플에서 단어 정보만 저장한다.
+        ner_tags.append(list(tag_info)) # 각 샘플에서 개체명 태깅 정보만 저장한다.
+
+    return sentences, ner_tags
+
+
+def tozenizer(sentences, ner_tags):
+    src_tokenizer = Tokenizer(oov_token='OOV') # 모든 단어를 사용하지만 인덱스 1에는 단어 'OOV'를 할당한다.
+    src_tokenizer.fit_on_texts(sentences)
+
+    tar_tokenizer = Tokenizer(lower=False) # 태깅 정보들은 내부적으로 대문자를 유지한채로 저장
+    tar_tokenizer.fit_on_texts(ner_tags)
+
+    return src_tokenizer, tar_tokenizer
+
+
+def biLSTM(input_dim=31819, output_dim=20, input_length=70, units=50, tag_size=18):
     model = Sequential()
     model.add(Embedding(input_dim=input_dim, output_dim=output_dim, input_length=input_length, mask_zero=True))
     model.add(Bidirectional(LSTM(units=units, return_sequences=True, recurrent_dropout=0.1)))
@@ -16,71 +55,54 @@ def biLSTM(input_dim, output_dim, input_length, units=50):
     crf = CRF(tag_size)
     model.add(crf)
 
+    model.compile(optimizer="rmsprop", loss=crf.loss_function, metrics=[crf.accuracy])
+
     return model
 
-dataPath = 'Data/entity-annotated-corpus/ner_dataset.csv'
 
-data = pd.read_csv(dataPath, encoding="latin1")
+def main():
+    dataPath = 'Data/entity-annotated-corpus/ner_dataset.csv'
 
-data = data.fillna(method="ffill")
-data['Word'] = data['Word'].str.lower()
+    data = load_data(dataPath)
+    sentences, ner_tags = preprocess_data(data)
 
-func = lambda temp: [(w, t) for w, t in zip(temp["Word"].values.tolist(), temp["Tag"].values.tolist())]
-tagged_sentences=[t for t in data.groupby("Sentence #").apply(func)]
-print("전체 샘플 개수: {}".format(len(tagged_sentences)))
+    src_tokenizer, tar_tokenizer = tozenizer(sentences, ner_tags)
 
-sentences, ner_tags = [], [] 
-for tagged_sentence in tagged_sentences: # 47,959개의 문장 샘플을 1개씩 불러온다.
-    sentence, tag_info = zip(*tagged_sentence) # 각 샘플에서 단어들은 sentence에 개체명 태깅 정보들은 tag_info에 저장.
-    sentences.append(list(sentence)) # 각 샘플에서 단어 정보만 저장한다.
-    ner_tags.append(list(tag_info)) # 각 샘플에서 개체명 태깅 정보만 저장한다.
+    vocab_size = len(src_tokenizer.word_index) + 1
+    tag_size = len(tar_tokenizer.word_index) + 1
+    print('단어 집합의 크기 : {}'.format(vocab_size))
+    print('개체명 태깅 정보 집합의 크기 : {}'.format(tag_size))
 
+    X_train = src_tokenizer.texts_to_sequences(sentences)
+    y_train = tar_tokenizer.texts_to_sequences(ner_tags)
 
-src_tokenizer = Tokenizer(oov_token='OOV') # 모든 단어를 사용하지만 인덱스 1에는 단어 'OOV'를 할당한다.
-src_tokenizer.fit_on_texts(sentences)
-tar_tokenizer = Tokenizer(lower=False) # 태깅 정보들은 내부적으로 대문자를 유지한채로 저장
-tar_tokenizer.fit_on_texts(ner_tags)
+    index_to_word = src_tokenizer.index_word
+    index_to_ner = tar_tokenizer.index_word
+    index_to_ner[0]='PAD'
 
-vocab_size = len(src_tokenizer.word_index) + 1
-tag_size = len(tar_tokenizer.word_index) + 1
-print('단어 집합의 크기 : {}'.format(vocab_size))
-print('개체명 태깅 정보 집합의 크기 : {}'.format(tag_size))
+    max_len = 70
+    X_train = pad_sequences(X_train, padding='post', maxlen=max_len)
+    y_train = pad_sequences(y_train, padding='post', maxlen=max_len)
 
-X_train = src_tokenizer.texts_to_sequences(sentences)
-y_train = tar_tokenizer.texts_to_sequences(ner_tags)
+    X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=.2, random_state=777)
 
-index_to_word = src_tokenizer.index_word
-index_to_ner = tar_tokenizer.index_word
-index_to_ner[0]='PAD'
+    y_train = to_categorical(y_train, num_classes=tag_size)
+    y_test = to_categorical(y_test, num_classes=tag_size)
 
-max_len = 70
-X_train = pad_sequences(X_train, padding='post', maxlen=max_len)
-y_train = pad_sequences(y_train, padding='post', maxlen=max_len)
+    print('훈련 샘플 문장의 크기 : {}'.format(X_train.shape))
+    print('훈련 샘플 레이블의 크기 : {}'.format(y_train.shape))
+    print('테스트 샘플 문장의 크기 : {}'.format(X_test.shape))
+    print('테스트 샘플 레이블의 크기 : {}'.format(y_test.shape))
 
-X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=.2, random_state=777)
+    model = biLSTM(input_dim=vocab_size, output_dim=20, input_length=max_len, units=50, tag_size=tag_size)
 
-y_train = to_categorical(y_train, num_classes=tag_size)
-y_test = to_categorical(y_test, num_classes=tag_size)
+    history = model.fit(X_train, y_train, batch_size=32, epochs=5, validation_split=0.1, verbose=1)
 
-print('훈련 샘플 문장의 크기 : {}'.format(X_train.shape))
-print('훈련 샘플 레이블의 크기 : {}'.format(y_train.shape))
-print('테스트 샘플 문장의 크기 : {}'.format(X_test.shape))
-print('테스트 샘플 레이블의 크기 : {}'.format(y_test.shape))
+    print("\n test accuracy: %.4f" % (model.evaluate(X_test, y_test)[1]))
 
-'''
-model = Sequential()
-model.add(Embedding(input_dim=vocab_size, output_dim=20, input_length=max_len, mask_zero=True))
-model.add(Bidirectional(LSTM(units=50, return_sequences=True, recurrent_dropout=0.1)))
-model.add(TimeDistributed(Dense(50, activation="relu")))
-crf = CRF(tag_size)
-model.add(crf)
-'''
+    model.save_weights('bi_lstm_crf_weight.h5')
 
-model = biLSTM(input_dim=vocab_size, output_dim=20, input_length=max_len, units=50)
+    return 0
 
-model.compile(optimizer="rmsprop", loss=crf.loss_function, metrics=[crf.accuracy])
-history = model.fit(X_train, y_train, batch_size=32, epochs=5, validation_split=0.1, verbose=1)
-
-print("\n test accuracy: %.4f" % (model.evaluate(X_test, y_test)[1]))
-
-model.save_weights('bi_lstm_crf_weight.h5')
+if __name__ == '__main__':
+    main()
---
+++ ner_tagging.py
@@ -0,0 +1,206 @@
+import numpy as np
+import re
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from biLSTM import load_data, preprocess_data, tozenizer, biLSTM
+
+
+def split_into_sentences(text):
+    """
+    This function can split the entire text of Huckleberry Finn into sentences in about 0.1 seconds
+    and handles many of the more painful edge cases that make sentence parsing non-trivial 
+    e.g. "Mr. John Johnson Jr. was born in the U.S.A but earned his Ph.D. in Israel before joining 
+    Nike Inc. as an engineer. He also worked at craigslist.org as a business analyst."
+    """
+    
+    alphabets= "([A-Za-z])"
+    prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+    suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+    starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+    acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+    websites = "[.](com|net|org|io|gov)"
+    
+    text = " " + text + "  "
+    text = text.replace("\n"," ")
+    text = re.sub(prefixes,"\\1<prd>",text)
+    text = re.sub(websites,"<prd>\\1",text)
+    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
+    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
+    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
+    if "”" in text: text = text.replace(".”","”.")
+    if "\"" in text: text = text.replace(".\"","\".")
+    if "!" in text: text = text.replace("!\"","\"!")
+    if "?" in text: text = text.replace("?\"","\"?")
+    text = text.replace("."," .<stop>")#     text = text.replace(".",".<stop>")
+    text = text.replace("?"," ?<stop>")#     text = text.replace("?","?<stop>")
+    text = text.replace("!"," !<stop>")#     text = text.replace("!","!<stop>")
+    text = text.replace("<prd>",".")
+    
+    text = text.replace('"', ' " ')
+    text = text.replace("\'s", " \'s")
+    text = text.replace(",", " ,")
+    
+    sentences = text.split("<stop>")
+    sentences = sentences[:-1]
+    sentences = [s.strip() for s in sentences]
+    
+    return sentences
+
+def process_ner(sentence, bio):
+    
+    joined = []
+    for w, pred in zip(sentence, bio):
+        joined.append((w,pred))
+    
+    i = 0
+    ner_list = []
+    while i < len(joined):
+        if joined[i][1] != 0 and joined[i][1] != 1:
+            ner = []
+            ner.append(joined[i])
+            i += 1
+
+            while joined[i][1] != 0 and joined[i][1] != 1:
+                ner.append(joined[i])
+                i += 1
+            
+            word = " ".join([x[0] for x in ner])
+            entity = ner[0][1]
+#             entity = index_to_ner[ner[0][1]]
+            ner_list.append((word, entity))
+        else:
+            i += 1
+    
+    return ner_list
+
+
+def ner_extract_from_text(model, text, word_to_index):
+    max_len = 70
+    ner_dict = {'Geographical Entity':[], 'Organization':[], 'Person':[], 'Geopolitical Entity':[],
+           'Time indicator':[], 'Artifact':[], 'Event':[], 'Natural Phenomenon':[]}
+    
+    for sentence in text:
+        new_sentence = sentence.lower().split()
+
+        new_X=[]
+        for w in new_sentence:
+            try:
+              new_X.append(word_to_index.get(w,1))
+            except KeyError:
+              new_X.append(word_to_index['OOV'])
+              # 모델이 모르는 단어에 대해서는 'OOV'의 인덱스인 1로 인코딩
+
+        pad_new = pad_sequences([new_X], padding="post", value=0, maxlen=max_len)
+
+        p = model.predict(np.array([pad_new[0]]))
+        p = np.argmax(p, axis=-1)
+
+        ner_list = process_ner(new_sentence, p[0])
+
+        for entity in ner_list:
+            if entity[1] == 2:
+                ner_dict['Geographical Entity'].append(entity[0])
+            elif entity[1] == 3:
+                ner_dict['Time indicator'].append(entity[0])
+            elif entity[1] == 4:
+                 ner_dict['Organization'].append(entity[0])
+            elif entity[1] == 6:
+                 ner_dict['Person'].append(entity[0])
+            elif entity[1] == 8:
+                 ner_dict['Geopolitical Entity'].append(entity[0])
+            elif entity[1] == 11:
+                 ner_dict['Artifact'].append(entity[0])
+            elif entity[1] == 12:
+                 ner_dict['Event'].append(entity[0])
+            elif entity[1] == 15:
+                 ner_dict['Natural Phenomenon'].append(entity[0])
+    
+    return ner_dict
+
+def ner_tagging(model_weights):
+    dataPath = 'Data/entity-annotated-corpus/ner_dataset.csv'
+
+    data = load_data(dataPath)
+    sentences, ner_tags = preprocess_data(data)
+
+    src_tokenizer, _ = tozenizer(sentences, ner_tags)
+
+    model = biLSTM()
+    model.load_weights(model_weights)
+
+print("start")
+
+text = ['The United States announced retaliatory sanctions on North Korea on Friday in response to the communist nation \'s alleged cyber-attacks on Sony Pictures , warning the actions are just the  " first aspect "  of its response .',
+ 'President Barack Obama signed an executive order authorizing additional sanctions on North Korean individuals and entities in response to the North \'s  " ongoing provocative , destabilizing , and repressive actions and policies , particularly its destructive and coercive cyber attack on Sony , "  the White House said in a statement .',
+ "Three North Korean entities and 10 officials were named in the sanctions , including the Reconnaissance General Bureau , Pyongyang 's primary intelligence organization , accused of arms trading and other activities banned under U.N. resolutions , according to the Treasury Department .",
+ 'Though those sanctioned are barred from using the U.S. financial system and U.S. citizens are banned from doing business with them , the measures are considered largely symbolic because the North has already been under a string of international sanctions and those newly sanctioned are not believed to have any dealings with the U.S. " We take seriously North Korea \'s attack that aimed to create destructive financial effects on a U.S. company and to threaten artists and other individuals with the goal of restricting their\xa0right to free expression , "  the White House said "  .',
+ "As the president has said , our response to North Korea 's attack against Sony Pictures Entertainment will be proportional , and will take place at a time and in a manner of our choosing .",
+ 'Today \'s actions are the first aspect of our response , "  it said .',
+ 'The FBI has determined that North Korea was behind the hack on Sony , confirming widespread suspicions pointing to the North that has expressed strong anger at a Sony movie ,  " The Interview , "  which involves a plot to assassinate North Korean leader Kim Jong-un .',
+ 'Obama has since vowed to  " respond proportionally "  to the attacks .',
+ 'North Korea has denied any responsibility , though it lauded the Sony hack as a  " righteous deed "  .',
+ '" The order is not targeted at the people of North Korea , but rather is aimed at the government of North Korea and its activities that threaten the United States and others , "  Obama said in a letter to House of Representatives and Senate leaders .',
+ 'The two other newly sanctioned North Korean entities are Korea Mining Development and Trading Corp .',
+ '(KOMID) and Korea Tangun Trading Corp .',
+ 'Eight of the 10 sanctioned individuals were KOMID officials stationed in Iran , Syria , Russia and Namibia .',
+ "KOMID is the North 's primary arms dealer and main exporter of goods and equipment related to ballistic missiles and conventional weapons , according to the Treasury Department .",
+ 'The company was previously sanctioned by the U.S. and the United Nations , it said .',
+ 'Korea Tangun Trading Corp .',
+ "is responsible for the procurement of commodities and technologies to support the North 's defense research and development program .",
+ 'The company was also a target of U.S. and U.N. sanctions , the department said .',
+ 'The sanctioned individuals include KOMID officials Kil Jong-hun , Kim Kwang-yon , Jang Song-chol , Kim Yong-chol , Jang Yong-son , Kim Kyu , Ryu Jin and Kang Ryong , as well as Yu Kwang-ho , a North Korean government official , and Kim Kwang-chun , a Tangun Trading Corp .',
+ 'official "  .',
+ 'Today \'s actions are driven by our commitment to hold North Korea accountable for its destructive and destabilizing conduct , "  Secretary of the Treasury Jacob Lew said in a statement .',
+ '" Even as the FBI continues its investigation into the cyber-attack against Sony Pictures Entertainment , these steps underscore that we will employ a broad set of tools to defend U.S. businesses and citizens , and to respond to attempts to undermine our values or threaten the national security of the United States "  .',
+ "The new sanctions also underline the confidence the U.S. has in blaming the North for the Sony hack despite growing doubts about the FBI 's finding among American cyber-security specialists .",
+ "Last week , a cyber-security firm , Norse , was reported to have briefed the FBI on the result of its own investigation that it was not North Korea , but laid-off Sony staff members that disrupted Sony 's computer network .",
+ 'On Friday , Scott Borg , director and chief economist of the U.S. Cyber Consequences Unit , an independent , nonprofit research institute specializing on cyber-threats and risks , also said in a commentary on the CNBC website that the skills employed in the Sony hack were too sophisticated for the North .']
+
+dataPath = 'Data/entity-annotated-corpus/ner_dataset.csv'
+model_weights = 'bi_lstm_crf_weight.h5'
+
+data = load_data(dataPath)
+sentences, ner_tags = preprocess_data(data)
+
+src_tokenizer, _ = tozenizer(sentences, ner_tags)
+word_to_index = src_tokenizer.word_index
+
+model = biLSTM()
+print("loading weight to bi-LSTM model...")
+model.load_weights(model_weights)
+
+
+print(ner_extract_from_text(model, text, word_to_index))
+
+def main():
+    nerDataPath = 'Data/entity-annotated-corpus/ner_dataset.csv'
+    newsDataPath = 'Data/news/'
+    model_weights = 'bi_lstm_crf_weight.h5'
+
+    datapaths = os.listdir(newsDataPath)
+
+    df = pd.DataFrame()
+
+    for p in datapaths:
+        with open(path + p, 'r') as f:
+            data = json.load(f)
+
+        dataframe = pd.DataFrame.from_dict(data)
+        df = df.append(dataframe)
+    
+
+    nerData = load_data(nerDataPath)
+    sentences, ner_tags = preprocess_data(data)
+
+    src_tokenizer, _ = tozenizer(sentences, ner_tags)
+    word_to_index = src_tokenizer.word_index
+
+    model = biLSTM()
+    print("loading weight to bi-LSTM model...")
+    model.load_weights(model_weights)
+
+
